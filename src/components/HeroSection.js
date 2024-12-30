d7e563c45f5f5f784c1fb2550979158e64cef24a
AnshulParkar <121174809+AnshulParkar@users.noreply.github.com>
2024-12-30 11:48:57 +0530

    StartByAp
    Basic Folders and files created

---
+++ src/components/HeroSection.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './HeroSection.css';
+
+const HeroSection = () => {
+  return (
+    <div className="hero-section text-center text-white">
+      <div className="overlay">
+        <h1>Contractors and Engineers for Facade and Roofing Works</h1>
+        <button className="btn btn-warning">Contact Us</button>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
