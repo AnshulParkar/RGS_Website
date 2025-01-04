52630e390c0e37bc7a0a32c06593f1fa331afd11
AnshulParkar <121174809+AnshulParkar@users.noreply.github.com>
2025-01-04 19:25:47 +0530

    changesAP

--- ROOP_GLASS/src/Components/footer.jsx
+++ ROOP_GLASS/src/Components/footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { MapPin, Mail, Phone } from 'lucide-react';
 import './footer.css';  // Change this line - remove "styles from"
-import businessCard from '../assets/PlatinumPartner.png'; // Import the image
+import businessCard from '../assets/PlatinumPartner2.png'; // Import the image
 
 const ContactSection = () => {
   return (
@@ -13,22 +13,25 @@ const ContactSection = () => {
           <div className="contactInfo">
             <div className="contactItem">
               <MapPin className="icon" />
-              <span className="contactText">ADDRESS</span>
+              <span className="contactText">Roop Glass Solution <br />A1 Business Center, 2nd Floor, Sundar Kapoor Compound,<br /> Opp Thakur Mall, Dashisar Check Nakka, Mumbai-401107.</span>
             </div>
             
             <div className="contactItem">
               <Mail className="icon" />
-              <span className="contactText">MAIL</span>
+              <span className="contactText">Roopglasss@gmail.com</span>
             </div>
             
             <div className="contactItem">
               <Phone className="icon" />
-              <span className="contactText">+91 0000000000</span>
+              <span className="contactText">+91 9320008279</span>
             </div>
           </div>
           
           <div className="cardContainer">
             <div className="card">
+                <br />
+                <br />
+                <br />
                 <img 
                     src={businessCard} 
                     alt="Business Card" 
@@ -37,6 +40,10 @@ const ContactSection = () => {
             </div>
           </div>
         </div>
+          <br />
+          <br />
+          <br />
+          <br />
       </div>
     </div>
   );
